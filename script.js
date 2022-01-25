5d12b8e66fadfc5ca4d639939b417ad9c4d72916
Robert Edmonds <bobby.edmonds89@gmail.com>
2022-01-24 22:11:17 -0700

    Added objects to db.json, changed the look on CSS background and moving buttons, added more versatility with JS

--- script.js
+++ script.js
@@ -5,6 +5,8 @@ function popDog(info){
     let dogName = document.createElement('h3')
     let dogAge = document.createElement('h3')
     let dogDiscript = document.createElement('h4')
+    let dogDonation = document.createElement('h4')
+    let dogLikes = document.createElement('h4')
     let donateBtn = document.createElement('button')
     let likeBtn = document.createElement('button')
     let adoptBtn = document.createElement('button')
@@ -16,27 +18,23 @@ function popDog(info){
     dogAge.textContent = `Age: ${info.age}`
     dogDiscript.classList.add('discription')
     dogDiscript.textContent = info.discription
+    dogLikes.classList.add('dog_likes')
+    dogLikes.textContent = `Likes: ${info.like}`
+    dogDonation.classList.add('dog_donations')
+    dogDonation.textContent = `Donations: ${info.donation}`
+    donateBtn.classList.add('donateBtn')
+    donateBtn.textContent = 'Donate $5'
+    likeBtn.classList.add('likeBtn')
+    likeBtn.textContent = 'Like'
+    adoptBtn.classList.add('adoptBtn')
+    adoptBtn.textContent = "Adopt Now!"
+    dogDiscript.append(dogLikes, dogDonation, donateBtn, likeBtn, adoptBtn)
     document.querySelector('.names').appendChild(titleName)
     titleBtn.addEventListener('click', event => {
         if(info.name === event.path[1].innerText){
             document.querySelector('.dog_info').replaceChildren(img, dogName, dogAge, dogDiscript)
         }
     })
-}
-function showDog(info){
-    let img = document.createElement('img')
-    let dogName = document.createElement('h3')
-    let dogAge = document.createElement('h3')
-    let dogDiscript = document.createElement('h4')
-    let donateBtn = document.createElement('button')
-    let likeBtn = document.createElement('button')
-    let adoptBtn = document.createElement('button')
-    img.src = info.image
-    dogName.textContent = `Name: ${info.name}`
-    dogAge.textContent = `Age: ${info.age}`
-    dogDiscript.classList.add('discription')
-    dogDiscript.textContent = info.discription
-    document.querySelector('.dog_info').replaceChildren(img, dogName, dogAge, dogDiscript)
 }
 function renderDog(){
     fetch('  http://localhost:3000/dogs')
